5532267e9d3a2301f99ae48cfe4e0d8058492bae
Yi3512 <2399283079@qq.com>
2022-07-13 12:15:14 +0800

    完成项目初始化

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+
 import store from './store'
+// 引入字体图标css
+import '@/assets/font/iconfont.css'
+// 引入flexble
+import 'amfe-flexible'
+// 引入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
